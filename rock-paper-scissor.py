31000f5b24346877eadeec813f19135d7b370124
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-05-23 20:28:46 -0400

    Update rock-paper-scissor.py

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -26,43 +27,35 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
-import random
+elements = [rock, paper, scissors]
 print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
 user_choice = int(input())
-elements = [rock, paper, scissors]
+print(elements[user_choice])
 computer_choice = random.choice(elements)
+print("Computer chose",computer_choice)
 
 
 #-----------------ROCK-----------------------------
 if user_choice == 0:
-    print("Computer chose",computer_choice)
-    print("Your chose",rock)
-    if computer_choice == computer_choice[0]:
-        print("Tie")
-    elif computer_choice == computer_choice[1]:
+    if computer_choice == elements[0]:
+        print("It's a draw")
+    elif computer_choice == elements[1]:
         print("You lose")
     else:
         print("You win")
 #------------------PAPER----------------------------
 elif user_choice == 1:
-    print("Computer chose",computer_choice)
-    print("Your chose",paper)
-    if computer_choice == computer_choice[0]:
+    if computer_choice == elements[0]:
        print("You win")
-    elif computer_choice == computer_choice[1]:
-         print("Tie")
+    elif computer_choice == elements[1]:
+         print("It's a draw")
     else:
         print("You lose")
 #------------------SCISSORS---------------------------
 elif user_choice == 2:
-    print("Computer chose",computer_choice)
-    print("Your chose",scissors)
-    if computer_choice == computer_choice[0]:
+    if computer_choice == elements[0]:
         print("You lose")
-    elif computer_choice == computer_choice[1]:
+    elif computer_choice == elements[1]:
         print("You win")
     else:
-        print("Tie")
-#------------------SCISSORS---------------------------
-else:
-    print("You put an invalid number, so you lose")
+        print("It's a draw")
