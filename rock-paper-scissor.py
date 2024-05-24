00eef00fb311c59691dc3e58ac755184161cf7ac
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-05-23 20:30:19 -0400

    Update rock-paper-scissor.py

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -25,40 +25,48 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-try:
-    #Write your code below this line 👇
-    elements = [rock, paper, scissors]
-    print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
-    user_choice = int(input())
-    print(elements[user_choice])
-    computer_choice = random.choice(elements)
-    print("Computer chose",computer_choice)
+answer = 0
+while answer == 0:
+    try:
+        #Write your code below this line 👇
+        elements = [rock, paper, scissors]
+        print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+        user_choice = int(input())
+        print(elements[user_choice])
+        computer_choice = random.choice(elements)
+        print("Computer chose",computer_choice)
 
 
-    #-----------------ROCK-----------------------------
-    if user_choice == 0:
-        if computer_choice == elements[0]:
-            print("It's a draw")
-        elif computer_choice == elements[1]:
-            print("You lose")
-        else:
-            print("You win")
-    #------------------PAPER----------------------------
-    elif user_choice == 1:
-        if computer_choice == elements[0]:
-            print("You win")
-        elif computer_choice == elements[1]:
-            print("It's a draw")
-        else:
-            print("You lose")
-    #------------------SCISSORS---------------------------
-    elif user_choice == 2:
-        if computer_choice == elements[0]:
-            print("You lose")
-        elif computer_choice == elements[1]:
-            print("You win")
-        else:
-            print("It's a draw")
-except:
-    print("You put an invalid number, so you lose")
+        #-----------------ROCK-----------------------------
+        if user_choice == 0:
+            if computer_choice == elements[0]:
+                print("It's a draw")
+            elif computer_choice == elements[1]:
+                print("You lose")
+            else:
+                print("You win")
+        #------------------PAPER----------------------------
+        elif user_choice == 1:
+            if computer_choice == elements[0]:
+                print("You win")
+            elif computer_choice == elements[1]:
+                print("It's a draw")
+            else:
+                print("You lose")
+        #------------------SCISSORS---------------------------
+        elif user_choice == 2:
+            if computer_choice == elements[0]:
+                print("You lose")
+            elif computer_choice == elements[1]:
+                print("You win")
+            else:
+                print("It's a draw")
+    except:
+        print("You put an invalid number, so you lose")
+    #----------------------START AGAIN----------------------------------
+    answer = input("Do you want to play again? Y/n ").lower()
+    if answer == "y":
+        answer = 0
+    else:
+        answer += 1
 
